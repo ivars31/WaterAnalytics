7c3175a44c49086ca48d44c5e1a9d35bcb95dd76
ivars31 <ivars31@gmail.com>
2014-05-05 22:39:09 -0400

    First graph obtained

--- InitialGraphs.py
+++ InitialGraphs.py
@@ -1,7 +1,31 @@
 __author__ = 'ivan'
-import matplotlib as m
+import matplotlib.pyplot as plt
 import MySQLdb
+from pylab import *
 
 db = MySQLdb.connect(host="localhost", user="root", passwd="ir", db="water_metrics")
-cur = db.cursor() #creating a cursor to execute queries
-cur.execute("SELECT * FROM daily_flow")
+#creating a cursor to execute queries
+cur = db.cursor()
+
+cur.execute("SELECT DAILY_TOTAL_FLOW, FLOW_DATE FROM daily_flow")
+result = cur.fetchall() #Obtaining all the result
+#cur.execute("SELECT FLOW_DATE FROM daily_flow")
+#date = cur.fetchall() #Obtaining all the result
+t = []
+s = []
+
+for record in result:
+  t.append(record[0])
+  s.append(record[1])
+
+plt.scatter(s, t, marker='*')
+plt.ylim([0, 45000])
+plt.show()
+
+#plot(t, s, 'ko')
+#axis([min(t), max(t), min(s), max(s)])
+#grid(True)
+#
+#F = gcf()
+#DPI = F.get_dpi()
+#F.savefig('plot.png',dpi = 80)
